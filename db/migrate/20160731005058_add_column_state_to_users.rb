40ca654536a897345363449afe2a063af10838ea
xinoon <carlos.alcalde.a@gmail.com>
2016-07-30 20:57:36 -0400

    ADD gem aasm y ADD migration column state

---
+++ db/migrate/20160731005058_add_column_state_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnStateToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :state, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730041923) do
+ActiveRecord::Schema.define(version: 20160731005058) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160730041923) do
     t.text     "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "state"
   end
 
 end
